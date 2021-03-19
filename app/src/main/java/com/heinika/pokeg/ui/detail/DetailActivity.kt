59585276eac3c1450ac718c6c984dc0a4565741d
chenlijin <Chenlijin@enovatemotors.com>
2021-03-19 16:04:33 +0800

    add detail page

--- app/src/main/java/com/heinika/pokeg/ui/detail/DetailActivity.kt
+++ app/src/main/java/com/heinika/pokeg/ui/detail/DetailActivity.kt
@@ -6,21 +6,33 @@ import android.widget.TextView
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import com.heinika.pokeg.R
+import com.heinika.pokeg.databinding.ActivityDetailBinding
+import com.heinika.pokeg.databinding.ActivityMainBinding
+import com.heinika.pokeg.model.PokemonInfo.Companion.maxHp
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class DetailActivity : AppCompatActivity() {
-
-
     private val viewModel: DetailViewModel by viewModels()
+
+    private lateinit var binding: ActivityDetailBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         window.requestFeature(Window.FEATURE_ACTIVITY_TRANSITIONS)
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_detail)
+        binding = ActivityDetailBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
+        binding.progressHp.max = maxHp.toFloat()
         viewModel.pokemonInfoLiveData.observe(this) {
+            binding.index.text = it.getIdString()
+            binding.weight.text = it.getWeightString()
+            binding.height.text = it.getHeightString()
+            binding.progressHp.progress = it.hp.toFloat()
+        }
 
+        binding.arrow.setOnClickListener {
+            finish()
         }
     }
 }
