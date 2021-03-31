259e76e3a8841d439bdc2b42486fe33b4af101bb
chenlijin <Chenlijin@enovatemotors.com>
2021-03-31 10:48:22 +0800

    move application oncreate() to start Up.

--- app/src/main/java/com/heinika/pokeg/init/TimberInitializer.kt
+++ app/src/main/java/com/heinika/pokeg/init/TimberInitializer.kt
@@ -5,6 +5,7 @@ import androidx.startup.Initializer
 import com.heinika.pokeg.BuildConfig
 import timber.log.Timber
 
+@Suppress("unused")
 class TimberInitializer : Initializer<Unit> {
     override fun create(context: Context) {
         if (BuildConfig.DEBUG) {
