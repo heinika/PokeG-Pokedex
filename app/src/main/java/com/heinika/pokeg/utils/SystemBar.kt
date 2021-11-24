ec220a277c1774f5a6e08bd9fbadc6a18be0db31
chenlijin <Chenlijin@enovatemotors.com>
2021-11-24 13:49:34 +0800

    Fix Bug.

--- app/src/main/java/com/heinika/pokeg/utils/SystemBar.kt
+++ app/src/main/java/com/heinika/pokeg/utils/SystemBar.kt
@@ -1,9 +1,6 @@
 package com.heinika.pokeg.utils
 
 import android.app.Activity
-import android.os.Build
-import android.view.WindowInsets
-import timber.log.Timber
 
 object SystemBar {
   var statusBarHeight: Int = 12.dp
@@ -11,27 +8,29 @@ object SystemBar {
   var navigationBarHeight = 0.dp
     private set
 
-  @Suppress("DEPRECATION")
+
   fun initStatusBarHeight(activity: Activity) {
-    val statusBarResourceId: Int = activity.resources.getIdentifier("status_bar_height", "dimen", "android")
+    val statusBarResourceId: Int =
+      activity.resources.getIdentifier("status_bar_height", "dimen", "android")
     if (statusBarResourceId > 0) {
       statusBarHeight = activity.resources.getDimensionPixelSize(statusBarResourceId)
     }
-    val navigationBarResourceId: Int = activity.resources.getIdentifier("navigation_bar_height", "dimen", "android")
+
+    val navigationBarResourceId: Int =
+      activity.resources.getIdentifier("navigation_bar_height", "dimen", "android")
     if (navigationBarResourceId > 0) {
       navigationBarHeight = activity.resources.getDimensionPixelSize(navigationBarResourceId)
     }
 
-    activity.window.decorView.setOnApplyWindowInsetsListener { _, insets ->
-      if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
-        statusBarHeight = insets.getInsets(WindowInsets.Type.systemBars()).top
-        navigationBarHeight = insets.getInsets(WindowInsets.Type.systemBars()).bottom
-      }else{
-        statusBarHeight = insets.systemWindowInsetTop
-        navigationBarHeight = insets.systemWindowInsetBottom
-      }
-      Timber.i("negationBarHeight:$navigationBarHeight")
-      return@setOnApplyWindowInsetsListener insets
-    }
+//    activity.window.decorView.setOnApplyWindowInsetsListener { _, insets ->
+//      if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+//        statusBarHeight = insets.getInsets(WindowInsets.Type.systemBars()).top
+//        navigationBarHeight = insets.getInsets(WindowInsets.Type.systemBars()).bottom
+//      } else {
+//        statusBarHeight = insets.systemWindowInsetTop
+//        navigationBarHeight = insets.systemWindowInsetBottom
+//      }
+//      return@setOnApplyWindowInsetsListener insets
+//    }
   }
 }
