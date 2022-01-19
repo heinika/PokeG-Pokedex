5d193fc8957ebb9f98ced8f293c702de70a34a1e
chenlijin <Chenlijin@enovatemotors.com>
2022-01-19 10:08:52 +0800

    Fix bug

--- app/src/main/java/com/heinika/pokeg/model/Team.kt
+++ app/src/main/java/com/heinika/pokeg/model/Team.kt
@@ -1,5 +1,8 @@
 package com.heinika.pokeg.model
 
+import com.squareup.moshi.JsonClass
+
+@JsonClass(generateAdapter = true)
 data class Team(
   val teamName: String,
   val teamNumbers: List<TeamNumber>
