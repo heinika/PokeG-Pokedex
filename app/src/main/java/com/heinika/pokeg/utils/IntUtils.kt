683efc99a4bfbf7c93301e0ee0b6adde52acd83f
chenlijin <Chenlijin@enovatemotors.com>
2022-03-16 16:45:50 +0800

    fix bug

--- app/src/main/java/com/heinika/pokeg/utils/IntUtils.kt
+++ app/src/main/java/com/heinika/pokeg/utils/IntUtils.kt
@@ -7,6 +7,5 @@ fun Int.toFormString(context: Context): String  = when(this){
     2 -> context.getString(R.string.alola)
     3 -> context.getString(R.string.galar)
     4 -> context.getString(R.string.hisui)
-    5 -> context.getString(R.string.white_stripes)
     else -> ""
   }
