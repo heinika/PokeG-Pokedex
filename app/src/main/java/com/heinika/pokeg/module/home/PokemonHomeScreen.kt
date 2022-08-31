70787572fa76a365087f5dc8887b833e5e7c46b6
chenlijin <chenlijin@enovatemotors.com>
2022-08-31 16:05:59 +0800

    Add Default Version Selected

--- app/src/main/java/com/heinika/pokeg/module/home/PokemonHomeScreen.kt
+++ app/src/main/java/com/heinika/pokeg/module/home/PokemonHomeScreen.kt
@@ -37,12 +37,14 @@ import coil.annotation.ExperimentalCoilApi
 import com.heinika.pokeg.*
 import com.heinika.pokeg.R
 import com.heinika.pokeg.info.DexType
+import com.heinika.pokeg.info.MoveVersion
 import com.heinika.pokeg.info.Type
 import com.heinika.pokeg.model.Pokemon
 import com.heinika.pokeg.module.main.MainViewModel
 import com.heinika.pokeg.module.moves.compose.ChipStatus
 import com.heinika.pokeg.module.moves.compose.SelectTwoTypeClipList
 import com.heinika.pokeg.module.mypokemon.compose.PokemonCard
+import com.heinika.pokeg.ui.compose.SelectVersionDialog
 import com.heinika.pokeg.ui.theme.*
 import com.heinika.pokeg.utils.SystemBar
 import kotlinx.coroutines.launch
@@ -64,205 +66,241 @@ fun PokemonHomeScreen(mainViewModel: MainViewModel, onDrawerItemClick: (screenNa
   val sortedPokemonList = mainViewModel.pokemonSortListLiveData.observeAsState()
   val scaffoldState = rememberScaffoldState()
   val scope = rememberCoroutineScope()
+  var moveVersion by remember { mutableStateOf(MoveVersion.values()[ConfigMMKV.defaultVersion - 1]) }
+  var isShowSelectVersionDialog by remember { mutableStateOf(false) }
 
-  Scaffold(
-    scaffoldState = scaffoldState,
-    drawerContent = {
-      LazyColumn(modifier = Modifier.padding(top = SystemBar.statusBarHeightDp.dp)) {
-        items(DrawerScreens.values()) {
-          Card(modifier = Modifier
-            .padding(12.dp, 6.dp)
-            .height(42.dp)
-            .fillMaxWidth(),
-            shape = MaterialTheme.shapes.small,
-            backgroundColor = it.color,
-            onClick = { onDrawerItemClick(it.screenName) }) {
-            Box {
-              Text(
-                text = stringResource(id = it.nameStringId),
-                modifier = Modifier.align(Alignment.Center),
-                textAlign = TextAlign.Center,
-                style = MaterialTheme.typography.body1
-              )
+  Box {
+    Scaffold(
+      scaffoldState = scaffoldState,
+      drawerContent = {
+        LazyColumn(modifier = Modifier.padding(top = SystemBar.statusBarHeightDp.dp)) {
+
+          item {
+            Card(modifier = Modifier
+              .padding(12.dp, 6.dp)
+              .height(42.dp)
+              .fillMaxWidth(),
+              shape = MaterialTheme.shapes.small,
+              backgroundColor = JollyColor,
+              onClick = { isShowSelectVersionDialog = true }
+            ) {
+              Box {
+                Text(
+                  text = "默认技能版本：${stringResource(moveVersion.stringId)}",
+                  modifier = Modifier.align(Alignment.Center),
+                  textAlign = TextAlign.Center,
+                  style = MaterialTheme.typography.body1
+                )
+              }
+            }
+          }
+
+          items(DrawerScreens.values()) {
+            Card(modifier = Modifier
+              .padding(12.dp, 6.dp)
+              .height(42.dp)
+              .fillMaxWidth(),
+              shape = MaterialTheme.shapes.small,
+              backgroundColor = it.color,
+              onClick = { onDrawerItemClick(it.screenName) }) {
+              Box {
+                Text(
+                  text = stringResource(id = it.nameStringId),
+                  modifier = Modifier.align(Alignment.Center),
+                  textAlign = TextAlign.Center,
+                  style = MaterialTheme.typography.body1
+                )
+              }
             }
           }
         }
-      }
-    }) { paddingValues ->
-    paddingValues.calculateTopPadding()
+      }) { paddingValues ->
+      paddingValues.calculateTopPadding()
 
-    val bottomDrawerState = rememberBottomDrawerState(initialValue = BottomDrawerValue.Closed)
-    sortedPokemonList.value?.let { sortedPokemonList ->
+      val bottomDrawerState = rememberBottomDrawerState(initialValue = BottomDrawerValue.Closed)
+      sortedPokemonList.value?.let { sortedPokemonList ->
 
-      val types = remember { mutableStateOf(emptyList<Type>()) }
-      BottomDrawer(
-        drawerState = bottomDrawerState,
-        gesturesEnabled = bottomDrawerState.isOpen,
-        drawerContent = {
-          MainBottomDrawer(types.value,
-            onTypeSelectedChange = { typeList ->
-              types.value = typeList
-              mainViewModel.filterTypeList = typeList.map { it.typeId }
-              mainViewModel.startSortAndFilter()
-            })
-        },
-      ) {
-        ConstraintLayout(modifier = Modifier.fillMaxSize()) {
-          val (pokemonColumn, actionsRow) = createRefs()
+        val types = remember { mutableStateOf(emptyList<Type>()) }
+        BottomDrawer(
+          drawerState = bottomDrawerState,
+          gesturesEnabled = bottomDrawerState.isOpen,
+          drawerContent = {
+            MainBottomDrawer(types.value,
+              onTypeSelectedChange = { typeList ->
+                types.value = typeList
+                mainViewModel.filterTypeList = typeList.map { it.typeId }
+                mainViewModel.startSortAndFilter()
+              })
+          },
+        ) {
+          ConstraintLayout(modifier = Modifier.fillMaxSize()) {
+            val (pokemonColumn, actionsRow) = createRefs()
 
-          val pokemonColumnState = rememberLazyListState()
-          var dexType by remember { mutableStateOf(DexType.Global) }
-          var isSearchMode by remember { mutableStateOf(false) }
-          var searchContent by remember { mutableStateOf("") }
-          val focusRequester = remember { FocusRequester() }
+            val pokemonColumnState = rememberLazyListState()
+            var dexType by remember { mutableStateOf(DexType.Global) }
+            var isSearchMode by remember { mutableStateOf(false) }
+            var searchContent by remember { mutableStateOf("") }
+            val focusRequester = remember { FocusRequester() }
 
-          val keyboardController = LocalSoftwareKeyboardController.current
+            val keyboardController = LocalSoftwareKeyboardController.current
 
-          LazyColumn(
-            modifier = Modifier.constrainAs(pokemonColumn) {
-              top.linkTo(parent.top)
-              start.linkTo(parent.start)
-            },
-            state = pokemonColumnState
-          ) {
-            item {
-              TopAppBar(
-                modifier = Modifier.padding(top = SystemBar.statusBarHeightDp.dp, start = 12.dp, end = 12.dp),
-                backgroundColor = Color.Transparent,
-                title = {
-                  Box {
-                    OutlinedTextField(
-                      value = searchContent,
-                      onValueChange = {
-                        searchContent = it
-                        mainViewModel.setSearchText(it)
-                      },
-                      modifier = Modifier
-                        .alpha(if (isSearchMode) 1f else 0f)
-                        .focusRequester(focusRequester),
-                      singleLine = true,
-                      keyboardOptions = KeyboardOptions(imeAction = ImeAction.Search),
-                      keyboardActions = KeyboardActions(onSearch = {
-                        keyboardController?.hide()
-                      }),
-                      placeholder = { Text(text = stringResource(id = androidx.appcompat.R.string.search_menu_title)) },
-                      colors = TextFieldDefaults.outlinedTextFieldColors(
-                        unfocusedBorderColor = Color.Transparent,
-                        focusedBorderColor = Color.Transparent,
-                        cursorColor = MaterialTheme.colors.primary
+            LazyColumn(
+              modifier = Modifier.constrainAs(pokemonColumn) {
+                top.linkTo(parent.top)
+                start.linkTo(parent.start)
+              },
+              state = pokemonColumnState
+            ) {
+              item {
+                TopAppBar(
+                  modifier = Modifier.padding(top = SystemBar.statusBarHeightDp.dp, start = 12.dp, end = 12.dp),
+                  backgroundColor = Color.Transparent,
+                  title = {
+                    Box {
+                      OutlinedTextField(
+                        value = searchContent,
+                        onValueChange = {
+                          searchContent = it
+                          mainViewModel.setSearchText(it)
+                        },
+                        modifier = Modifier
+                          .alpha(if (isSearchMode) 1f else 0f)
+                          .focusRequester(focusRequester),
+                        singleLine = true,
+                        keyboardOptions = KeyboardOptions(imeAction = ImeAction.Search),
+                        keyboardActions = KeyboardActions(onSearch = {
+                          keyboardController?.hide()
+                        }),
+                        placeholder = { Text(text = stringResource(id = androidx.appcompat.R.string.search_menu_title)) },
+                        colors = TextFieldDefaults.outlinedTextFieldColors(
+                          unfocusedBorderColor = Color.Transparent,
+                          focusedBorderColor = Color.Transparent,
+                          cursorColor = MaterialTheme.colors.primary
+                        )
                       )
-                    )
 
-                    if (!isSearchMode) {
-                      Row(modifier = Modifier
-                        .fillMaxSize()
-                        .padding(0.dp, 8.dp)
-                        .clickable { }, verticalAlignment = Alignment.CenterVertically
-                      ) {
-                        Text(stringResource(id = dexType.stringId))
-                      }
-                    }
-                  }
-                },
-                actions = {
-                  if (!isSearchMode) {
-                    Icon(imageVector = Icons.Default.LocationOn, contentDescription = "", modifier = Modifier
-                      .clickable {
-                        dexType = dexType.next()
-                        mainViewModel.changDexType(dexType)
-                        mainViewModel.startSortAndFilter()
-                      }
-                      .padding(12.dp))
-                  }
-
-                  Icon(imageVector = Icons.Default.Search, contentDescription = "", modifier = Modifier
-                    .clickable {
-                      isSearchMode = true
-                      focusRequester.requestFocus()
-                      keyboardController?.show()
-                    }
-                    .padding(12.dp))
-                },
-                navigationIcon = {
-                  if (isSearchMode) {
-                    Icon(imageVector = Icons.Default.ArrowBack, contentDescription = "", modifier = Modifier
-                      .clickable {
-                        isSearchMode = false
-                        keyboardController?.hide()
-                      }
-                      .padding(12.dp))
-                  } else {
-                    Icon(imageVector = Icons.Default.Menu, contentDescription = "", modifier = Modifier
-                      .clickable {
-                        scope.launch {
-                          scaffoldState.drawerState.open()
+                      if (!isSearchMode) {
+                        Row(
+                          modifier = Modifier
+                            .fillMaxSize()
+                            .padding(0.dp, 8.dp)
+                            .clickable { }, verticalAlignment = Alignment.CenterVertically
+                        ) {
+                          Text(stringResource(id = dexType.stringId))
                         }
                       }
+                    }
+                  },
+                  actions = {
+                    if (!isSearchMode) {
+                      Icon(imageVector = Icons.Default.LocationOn, contentDescription = "", modifier = Modifier
+                        .clickable {
+                          dexType = dexType.next()
+                          mainViewModel.changDexType(dexType)
+                          mainViewModel.startSortAndFilter()
+                        }
+                        .padding(12.dp))
+                    }
+
+                    Icon(imageVector = Icons.Default.Search, contentDescription = "", modifier = Modifier
+                      .clickable {
+                        isSearchMode = true
+                        focusRequester.requestFocus()
+                        keyboardController?.show()
+                      }
                       .padding(12.dp))
-                  }
-                })
-            }
-
-            items(sortedPokemonList) { pokemon ->
-              PokemonCard(pokemon = pokemon, onClick = { onPokemonItemClick(pokemon) })
-            }
-          }
-
-          AnimatedVisibility(
-            visible = !pokemonColumnState.isScrollInProgress,
-            enter = fadeIn() + slideIn(initialOffset = {
-              IntOffset(
-                0,
-                SystemBar.statusBarHeightDp.toInt()
-              )
-            }),
-            exit = slideOut(targetOffset = {
-              IntOffset(
-                0,
-                SystemBar.statusBarHeightDp.toInt()
-              )
-            }) + fadeOut(),
-            modifier = Modifier.constrainAs(actionsRow) {
-              bottom.linkTo(parent.bottom, Dp(SystemBar.navigationBarHeightDp))
-              start.linkTo(parent.start)
-              end.linkTo(parent.end)
-            }) {
-            Row(horizontalArrangement = Arrangement.SpaceEvenly, modifier = Modifier.fillMaxWidth()) {
-
-              FloatingActionButton(
-                onClick = {
-                  scope.launch {
-                    pokemonColumnState.scrollToItem(0)
-                  }
-                }
-              ) {
-                Image(
-                  painter = painterResource(id = R.drawable.ic_arrow_top),
-                  contentDescription = ""
-                )
+                  },
+                  navigationIcon = {
+                    if (isSearchMode) {
+                      Icon(imageVector = Icons.Default.ArrowBack, contentDescription = "", modifier = Modifier
+                        .clickable {
+                          isSearchMode = false
+                          keyboardController?.hide()
+                        }
+                        .padding(12.dp))
+                    } else {
+                      Icon(imageVector = Icons.Default.Menu, contentDescription = "", modifier = Modifier
+                        .clickable {
+                          scope.launch {
+                            scaffoldState.drawerState.open()
+                          }
+                        }
+                        .padding(12.dp))
+                    }
+                  })
               }
 
-              FloatingActionButton(
-                onClick = {
-                  scope.launch {
-                    bottomDrawerState.open()
-                  }
-                }
-              ) {
-                Image(
-                  painter = painterResource(id = R.drawable.ic_filter_list),
-                  contentDescription = ""
-                )
+              items(sortedPokemonList) { pokemon ->
+                PokemonCard(pokemon = pokemon, onClick = { onPokemonItemClick(pokemon) })
               }
             }
 
+            AnimatedVisibility(
+              visible = !pokemonColumnState.isScrollInProgress,
+              enter = fadeIn() + slideIn(initialOffset = {
+                IntOffset(
+                  0,
+                  SystemBar.statusBarHeightDp.toInt()
+                )
+              }),
+              exit = slideOut(targetOffset = {
+                IntOffset(
+                  0,
+                  SystemBar.statusBarHeightDp.toInt()
+                )
+              }) + fadeOut(),
+              modifier = Modifier.constrainAs(actionsRow) {
+                bottom.linkTo(parent.bottom, Dp(SystemBar.navigationBarHeightDp))
+                start.linkTo(parent.start)
+                end.linkTo(parent.end)
+              }) {
+              Row(horizontalArrangement = Arrangement.SpaceEvenly, modifier = Modifier.fillMaxWidth()) {
+
+                FloatingActionButton(
+                  onClick = {
+                    scope.launch {
+                      pokemonColumnState.scrollToItem(0)
+                    }
+                  }
+                ) {
+                  Image(
+                    painter = painterResource(id = R.drawable.ic_arrow_top),
+                    contentDescription = ""
+                  )
+                }
+
+                FloatingActionButton(
+                  onClick = {
+                    scope.launch {
+                      bottomDrawerState.open()
+                    }
+                  }
+                ) {
+                  Image(
+                    painter = painterResource(id = R.drawable.ic_filter_list),
+                    contentDescription = ""
+                  )
+                }
+              }
+
+            }
+
+
           }
-
-
         }
       }
     }
 
+    SelectVersionDialog(
+      dialogState = isShowSelectVersionDialog,
+      onDialogStateChange = { isShowSelectVersionDialog = it },
+      onDismissRequest = { isShowSelectVersionDialog = false },
+      moveVersionList = MoveVersion.values().toList(),
+      onVersionItemClick = {
+        ConfigMMKV.defaultVersion = it.id
+        moveVersion = it
+        isShowSelectVersionDialog = false
+      })
   }
 }
 
