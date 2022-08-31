dc21e2bec3cf52d981f9922df2e3926f1d891cd7
chenlijin <chenlijin@enovatemotors.com>
2022-08-31 16:07:51 +0800

    remove unused import

--- app/src/main/java/com/heinika/pokeg/ui/compose/SelectVersionDialog.kt
+++ app/src/main/java/com/heinika/pokeg/ui/compose/SelectVersionDialog.kt
@@ -3,7 +3,6 @@ package com.heinika.pokeg.ui.compose
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
-import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
@@ -13,8 +12,6 @@ import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.DialogProperties
 import com.heinika.pokeg.info.MoveVersion
-import com.heinika.pokeg.info.Nature
-import com.heinika.pokeg.info.Version
 
 @ExperimentalMaterialApi
 @Composable
