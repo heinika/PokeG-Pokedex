c2f4e8353b42b6fb2bfa27fa983718e240cb29bb
chenlijin <chenlijin@enovatemotors.com>
2022-09-13 15:16:44 +0800

    Add About Page.

--- app/src/main/java/com/heinika/pokeg/module/about/AboutScreen.kt
+++ app/src/main/java/com/heinika/pokeg/module/about/AboutScreen.kt
@@ -1,20 +1,24 @@
 package com.heinika.pokeg.module.about
 
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.material.Text
+import android.content.ActivityNotFoundException
+import android.content.Intent
+import android.net.Uri
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material.icons.filled.Favorite
 import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.heinika.pokeg.BuildConfig
 import com.heinika.pokeg.R
 import com.heinika.pokeg.utils.SystemBar
 
@@ -23,8 +27,8 @@ import com.heinika.pokeg.utils.SystemBar
 fun AboutScreen(onBack: () -> Unit) {
   Scaffold(
     topBar = {
-      CenterAlignedTopAppBar(
-        modifier = Modifier.padding(top = SystemBar.statusBarHeightDp.dp),
+      SmallTopAppBar(
+        modifier = Modifier.padding(top = SystemBar.statusBarHeightDp.dp, start = 12.dp, end = 12.dp),
         title = {
           Text(
             stringResource(id = R.string.app_name),
@@ -47,25 +51,114 @@ fun AboutScreen(onBack: () -> Unit) {
               contentDescription = "Localized description"
             )
           }
-        }
+        },
+        scrollBehavior = null
       )
     },
     content = { innerPadding ->
-      LazyColumn(
-        contentPadding = innerPadding,
-        verticalArrangement = Arrangement.spacedBy(8.dp)
+
+      Column(
+        Modifier
+          .fillMaxWidth()
+          .padding(innerPadding),
+        horizontalAlignment = Alignment.CenterHorizontally
       ) {
-        val list = (0..75).map { it.toString() }
-        items(count = list.size) {
-          Text(
-            text = list[it],
-            style = MaterialTheme.typography.bodyLarge,
-            modifier = Modifier
-              .fillMaxWidth()
-              .padding(horizontal = 16.dp)
-          )
+        Image(
+          painter = painterResource(id = R.drawable.ic_launcher_foreground),
+          contentDescription = "",
+          modifier = Modifier
+            .size(188.dp)
+            .padding(24.dp)
+        )
+        Text(text = "PokeG By heinika")
+        Text(text = "v${BuildConfig.VERSION_NAME}")
+
+        Card(
+          modifier = Modifier
+            .fillMaxWidth()
+            .padding(12.dp)
+        ) {
+          Text(text = "介绍与帮助", modifier = Modifier.padding(12.dp))
+          Text(text = stringResource(id = R.string.app_desc), modifier = Modifier.padding(start = 12.dp, end = 12.dp, bottom = 12.dp))
+        }
+
+        val context = LocalContext.current
+        Card(
+          modifier = Modifier
+            .fillMaxWidth()
+            .height(108.dp)
+            .padding(12.dp),
+          onClick = {
+            val intent = Intent(Intent.ACTION_VIEW)
+            intent.data = Uri.parse("http://weibo.com/u/6222257860")
+            try {
+              context.startActivity(intent)
+            } catch (e: ActivityNotFoundException) {
+              e.printStackTrace()
+            }
+          }
+        ) {
+          Row {
+            Image(painter = painterResource(id = R.drawable.about_avatar), contentDescription = "")
+            Column(Modifier.fillMaxHeight(), verticalArrangement = Arrangement.Center) {
+              Text(text = "乌班图说", modifier = Modifier.padding(12.dp), style = MaterialTheme.typography.titleMedium)
+              Text(
+                text = "Developer & 好奇猫",
+                modifier = Modifier.padding(start = 12.dp, end = 12.dp, bottom = 12.dp),
+                style = MaterialTheme.typography.bodyMedium
+              )
+            }
+          }
+        }
+
+        Card(
+          modifier = Modifier
+            .fillMaxWidth()
+            .height(108.dp)
+            .padding(12.dp),
+          onClick = {
+            val intent = Intent(Intent.ACTION_VIEW)
+            intent.data = Uri.parse("https://heinika.github.io")
+            try {
+              context.startActivity(intent)
+            } catch (e: ActivityNotFoundException) {
+              e.printStackTrace()
+            }
+          }
+        ) {
+          Row {
+            Image(painter = painterResource(id = R.drawable.chenlijin), contentDescription = "")
+            Column(Modifier.fillMaxHeight(), verticalArrangement = Arrangement.Center) {
+              Text(text = "Developer", modifier = Modifier.padding(12.dp), style = MaterialTheme.typography.titleMedium)
+              Text(
+                text = "Heinika",
+                modifier = Modifier.padding(start = 12.dp, end = 12.dp, bottom = 12.dp),
+                style = MaterialTheme.typography.bodyMedium
+              )
+            }
+          }
+        }
+
+        Card(
+          modifier = Modifier
+            .fillMaxWidth()
+            .height(108.dp)
+            .padding(12.dp)
+        ) {
+          Row {
+            Image(painter = painterResource(id = R.drawable.kabi), contentDescription = "")
+            Column(Modifier.fillMaxHeight(), verticalArrangement = Arrangement.Center) {
+              Text(text = "卡比兽", modifier = Modifier.padding(12.dp), style = MaterialTheme.typography.titleMedium)
+              Text(
+                text = "陪睡员",
+                modifier = Modifier.padding(start = 12.dp, end = 12.dp, bottom = 12.dp),
+                style = MaterialTheme.typography.bodyMedium
+              )
+            }
+          }
         }
       }
+
     }
   )
 }
